eef62f03421974e5076ca821010abb96ad832abf
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2022-08-05 09:32:14 -0300

    SpringBoot Aula 2.5...

--- SpringBootAPI/vendas/src/main/java/br/com/tiacademy/vendas/controller/VendedorController.java
+++ SpringBootAPI/vendas/src/main/java/br/com/tiacademy/vendas/controller/VendedorController.java
@@ -1,37 +1,56 @@
 package br.com.tiacademy.vendas.controller;
 
 import br.com.tiacademy.vendas.domain.Vendedor;
+import br.com.tiacademy.vendas.service.VendedorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController
 @RequestMapping("/vendedor")
 public class VendedorController {
 
+    @Autowired
+    private VendedorService vendedorService;
+
     @GetMapping
-    public ResponseEntity<List<Vendedor>> listar(){
-
-        Vendedor um = new Vendedor();
-        um.setId(1L);
-        um.setNome("João");
-
-        Vendedor dois = new Vendedor();
-        um.setId(2L);
-        um.setNome("Manuel");
-
-        var vendedores = Arrays.asList(um, dois);
+    public ResponseEntity<List<Vendedor>> listar() {
+        var vendedores = vendedorService.listar();
         return ResponseEntity.ok(vendedores);
-
-
-
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Vendedor> especifico(@PathVariable("id") Long id){
+
+        var resultado = vendedorService.porId(id);
+
+        if (Objects.isNull(resultado)){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(resultado);
+    }
+
+    @PostMapping
+    public ResponseEntity<Vendedor> criar(@RequestBody Vendedor vendedor){
+        var salvo = vendedorService.criar(vendedor);
+        return ResponseEntity.ok(salvo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Vendedor> editar(@PathVariable("id") Long id,@RequestBody Vendedor vendedor){
+        return ResponseEntity.ok(vendedorService.editar(id, vendedor));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+        vendedorService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
 
 
 }
--- SpringBootAPI/vendas/src/main/java/br/com/tiacademy/vendas/domain/Vendedor.java
+++ SpringBootAPI/vendas/src/main/java/br/com/tiacademy/vendas/domain/Vendedor.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 public class Vendedor {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 
